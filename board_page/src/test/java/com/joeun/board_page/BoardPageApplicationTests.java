fa5052cb2d0772ecae584c5b3273009602b334b0
yunazz2 <z____yn@naver.com>
2024-01-11 11:43:54 +0900

    ⛄ flutter 연결을 위한 board_page 생성

---
+++ board_page/src/test/java/com/joeun/board_page/BoardPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joeun.board_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
